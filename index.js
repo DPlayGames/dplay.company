1bfd2c9c3a77811132fb1043a6f753254c697e97
Hanul <hanul@hanul.me>
2019-08-14 17:23:05 +0900

    언어 교체 기능

--- index.js
+++ index.js
@@ -48,4 +48,73 @@ RUN(() => {
 			memeberPanel.fireEvent('mouseover');
 		}
 	});
+	
+	let langStore = STORE('langStore');
+	
+	let browserLang = MSG({
+		en : 'en',
+		ko : 'ko',
+		'zh-TW' : 'zh-TW',
+		'zh-CN' : 'zh-CN'
+	});
+	
+	let changePageByLang = (lang) => {
+		
+		if (lang === 'en') {
+			location.href = 'index.html';
+		}
+		
+		if (lang === 'ko') {
+			location.href = 'index-kr.html';
+		}
+		
+		if (lang === 'zh-TW') {
+			location.href = 'index-zh-tw.html';
+		}
+		
+		if (lang === 'zh-CN') {
+			location.href = 'index-zh-cn.html';
+		}
+	};
+	
+	let changeLangSelect = SELECT({
+		style : {
+			position : 'absolute',
+			right : 10,
+			top : 10,
+			backgroundColor : '#1e1e1e',
+			color : '#666',
+			padding : '5px 8px',
+			border : 'none'
+		},
+		c : [OPTION({
+			value : 'en',
+			c : 'English'
+		}), OPTION({
+			value : 'ko',
+			c : '한국어'
+		}), OPTION({
+			value : 'zh-TW',
+			c : '繁體中文'
+		}), OPTION({
+			value : 'zh-CN',
+			c : '简体中文'
+		})],
+		value : pageLang,
+		on : {
+			change : (e, select) => {
+				changePageByLang(select.getValue());
+			}
+		}
+	}).appendTo(BODY);
+	
+	if (langStore.get('lang') === undefined) {
+		
+		langStore.save({
+			name : 'lang',
+			value : browserLang
+		});
+		
+		changePageByLang(browserLang);
+	}
 });
